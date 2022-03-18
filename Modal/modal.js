584b3e2460497e1ebfd8153a235d5be199c1a30b
BarisGulyuz <bar.77@windowslive.com>
2022-03-19 00:47:00 +0300

    .

--- Modal/modal.js
+++ Modal/modal.js
@@ -1,6 +1,5 @@
 const button = document.querySelector('.modal-button')
 const modal = document.querySelector('.bg-modal')
-const closeButton = document.querySelector('.close')
 
 button.addEventListener('click', () => {
     modal.showModal()
